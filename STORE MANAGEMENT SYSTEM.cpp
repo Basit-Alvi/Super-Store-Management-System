0feaba28b6ad2a7dfa80c87bf867c26953212d8c
M Basit Ali <basitaly333@gmail.com>
2026-06-18 16:54:16 +0500

    Update STORE MANAGEMENT SYSTEM.cpp

--- STORE MANAGEMENT SYSTEM.cpp
+++ STORE MANAGEMENT SYSTEM.cpp
@@ -1,6 +1,7 @@
  #include <iostream>     // For input/output
 #include <fstream>      // For file handling
 #include <string>       // For string.
+#include<algorithm>
 using namespace std;
 
 /* ---------- STRUCTURE FOR PRODUCT ----------- */
